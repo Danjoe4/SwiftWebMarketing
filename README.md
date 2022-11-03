06f560a6dc017152d4b2f313a01ee747dd1b6e61
Danjoe4 <djbroderick4@gmail.com>
2022-11-03 03:35:16 -0400

    SSL cert and prod refinement

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-based on
+# based on
 https://github.com/uideck/simple-bootstrap-5-template
+# guide for prod
+https://dev.to/thetrebelcc/how-to-run-a-flask-app-over-https-using-waitress-and-nginx-2020-235c
 
+
+# handy, gets pid associated with port 
+lsof -i :5000 
+# then 
+kill pid
+
+# To connect #####
+ssh -i .ssh/swiftwebmarketing_home_site.pem ubuntu@ec2-100-27-11-64.compute-1.amazonaws.com 
+# add changes 
+git fetch --all 
+git reset --hard origin/main
+# make a new detached session
+sudo tmux new -s session
+# or attach to existing session
+sudo tmux attach -t session
+# in tmux session
+source env/bin/activate
+# start the app
+python3 app.py --prod
+# Press Ctrl B and press D to close the session
+
+# nginx 
+sudo systemctl status nginx
+make sure it is working
--- app.py
+++ app.py
@@ -19,16 +19,53 @@ def send_email():
     recipient = "contact@swiftwebmarketing.com"
 
     return ""
-    
+
+def config_email():
+    """ errors go to email via SMTP
+    """
+    Username = "AKIAVONITVAWGURN2AMM"
+    Password = "BE58OAUURsmyTX5S4kjOUiMGzFk5P172r9dxUzaQMFka"
+
+
+    mail_handler = SMTPHandler(
+    mailhost='127.0.0.1',
+    fromaddr='server-error@example.com',
+    toaddrs=['alerts+swb@swiftwebmarketing.com'],
+    subject='Application Error for Swift Web Marketing main site'
+    )
+    mail_handler.setLevel(logging.INFO)
+    mail_handler.setFormatter(logging.Formatter(
+        '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+    ))
+    return mail_handler
+
 def create_app():
     """ production settings
     """
+    # add logging
     import logging
     logging.basicConfig(filename='error.log',level=logging.INFO)
-    
+    # we use nginx reverse proxy to handle SSL
+    from werkzeug.middleware.proxy_fix import ProxyFix
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
+    # add email alerts
+    # mail_handler = config_email()
+    #app.logger.addHandler(mail_handler)
     return app
 
 
+
+def run_app():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--prod')
+    args = parser.parse_args()
+    if args.prod:
+        app = create_app()
+        from waitress import serve
+        serve(app, host='127.0.0.1', port=5000, url_scheme='https') 
+    else:
+        app.run(debug=True) # for local testing
+
 if __name__ == '__main__':
-    #app= create_app() # run in production mode locally
-    app.run(debug=True) # for local testing
+    run_app()
